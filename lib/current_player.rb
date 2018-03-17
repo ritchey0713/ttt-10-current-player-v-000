bef7cb48d61df09a1890ff01b5a0b0c092a1eebe
David Ritchey <ritchey0713@gmail.com>
2018-03-17 22:47:39 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,6 @@ def turn_count(board)
 end 
 
 def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  else
-    return "O"
-  end
-end
+   turn_count(board).even? ? "X" : "O"
+ end
 
